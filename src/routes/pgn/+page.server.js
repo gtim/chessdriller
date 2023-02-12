f590b84f41386b8cc1cb71a6613b98c11afa33da
Tim N <tim@gurka.se>
2023-02-12 16:46:18 +0100

    handle multi-PGN files

--- src/routes/pgn/+page.server.js
+++ src/routes/pgn/+page.server.js
@@ -9,25 +9,32 @@ export const actions = {
 		const forWhite = false;
 		// TODO: check file size
 
-		const pgntext_orig = await file.text();
-		const pgntext_fixed = pgntext_orig.replace(/\*\s*$/, '\n'); // remove invalid(?) final '*' placed by Lichess study export
-		const chess = new Chess();
-		chess.loadPgn( pgntext_fixed );
-		// TODO: handle unparsable PGNs
-
+		const pgndb = await file.text();
+		const pgntexts = split_pgndb_into_pgns( pgndb );
 		const pgn = await prisma.pgn.create({ data: {
 			userId: 1, //TODO
 			forWhite: forWhite,
 			filename: file.name,
-			content: pgntext_orig
+			content: pgndb
 		} });
-
-		await insert_all_moves( pgn.id, forWhite, chess.history() );
+		for ( const pgntext of pgntexts ) {
+			const pgntext_fixed = pgntext.replace(/\*\s*$/, '\n'); // remove final '*', maybe cm-chess/chess.js bug makes it cause parser issues?
+			const chess = new Chess();
+			chess.loadPgn( pgntext_fixed ); // TODO: handle unparsable PGNs
+			await insert_all_moves( pgn.id, forWhite, chess.history() );
+		}
 
 		return { success: true };
 	}
 };
 
+// A PGN database file can contain multiple PGNs: http://www.saremba.de/chessgml/standards/pgn/pgn-complete.htm#c8
+// Since chess.js and cm-chess don't support multiple PGNs, the file is split here. The spec points out that it is simple enough that a full-blown parser is not needed, but it remains to be seen whether kinda-complying PGNs in the wild will cause trouble. Ideally, this would be solved in cm-chess (or chess.js).
+function split_pgndb_into_pgns( pgn_db ) {
+	const regex = /(\[.*?\n\n *\S.*?\n)/gs;
+	const found = pgn_db.match(regex);
+	return found;
+}
 
 // Canonicalise FEN by removing last three elements: en passant square, half-move clock and fullmove number. see README
 function normalize_fen( fen ) {
