72ee8c6c4b0c0f1810d096ba4f85a17eda8218e4
Tim N <tim@gurka.se>
2023-02-17 13:08:42 +0100

    vitest preparation

---
+++ test/sample.spec.js
@@ -0,0 +1,6 @@
+describe("sample suite", () => {
+	test("sample test", () => {
+		expect(1 + 1).equal(2);
+	});
+});
+
---
+++ vitest.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit({ hot: !process.env.VITEST })],
+	test: {
+		globals: true,
+	}
+};
+
+export default config;
+
