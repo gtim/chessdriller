c8a3fb0d875f227dbb274c50d0c0d48689fa5515
Tim N <tim@gurka.se>
2023-02-20 12:29:37 +0100

    scheduler test: make sure most due move is returned

--- test/scheduler.test.js
+++ test/scheduler.test.js
@@ -101,13 +101,13 @@ describe( 'getNextLineForStudy', () => {
 		).toMatchObject( empty_response );
 	} );
 
-	test( 'no moves at all', async () => {
+	test( 'with no moves at all, empty response should be returned', async () => {
 		expect(
 			await getNextLineForStudy( [], new Date('2023-01-21T12:34:56') )
 		).toMatchObject( empty_response );
 	} );
 
-	test( 'no own moves', async () => {
+	test( 'with no own moves, empty response should be returned', async () => {
 		const moves = [
 			{ id: 0, moveSan: 'e4', ownMove: false, repForWhite: false,
 			  fromFen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq',
@@ -122,7 +122,67 @@ describe( 'getNextLineForStudy', () => {
 			await getNextLineForStudy( moves, new Date('2023-01-28T00:00:00') )
 		).toMatchObject( empty_response );
 	} );
-	test.todo( 'no last_line: find the most due move (review-only and mixed)' );
+
+	test( 'with no last_line, the most due move should be returned', async () => {
+		const moves = [
+			{ id: 0, moveSan: 'e4', ownMove: true, repForWhite: true,
+			  fromFen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq',
+			  toFen: 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq' },
+			{ id: 1, moveSan: 'd4', ownMove: true, repForWhite: true,
+			  fromFen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq',
+			  toFen: 'rnbqkbnr/pppppppp/8/8/3P4/8/PPP1PPPP/RNBQKBNR b KQkq' },
+			{ id: 2, moveSan: 'c4', ownMove: true, repForWhite: true,
+			  fromFen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq',
+			  toFen: 'rnbqkbnr/pppppppp/8/8/2P5/8/PP1PPPPP/RNBQKBNR b KQkq' },
+			{ id: 3, moveSan: 'Nf3', ownMove: true, repForWhite: true,
+			  fromFen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq',
+			  toFen: 'rnbqkbnr/pppppppp/8/8/8/5N2/PPPPPPPP/RNBQKB1R b KQkq' }
+		];
+		for ( let i = 0; i < moves.length; i++ ) {
+
+			// learning only
+			moves.forEach( m => {
+				m.learningDueTime = new Date('2023-01-21T12:34:56' );
+				m.reviewDueDate = null;
+			} );
+			moves[i].learningDueTime = new Date('2023-01-21T12:34:55' );
+			expect(
+				await getNextLineForStudy( moves, new Date('2023-01-21T12:34:57') )
+			).toMatchObject( {
+				line: [ { id: i } ],
+				start_ix: 0
+			} );
+
+			// review only
+			moves.forEach( m => {
+				m.learningDueTime = null;
+				m.reviewDueDate = new Date('2023-01-21T00:00:00' );
+			} );
+			moves[i].reviewDueDate = new Date('2023-01-20T00:00:00' );
+			expect(
+				await getNextLineForStudy( moves, new Date('2023-01-21T12:34:57') )
+			).toMatchObject( {
+				line: [ { id: i } ],
+				start_ix: 0
+			} );
+
+			// mixed learning/review
+			moves[i].learningDueTime = new Date('2023-01-21T12:34:56' );
+			moves[i].reviewDueDate = null;
+			moves[(i+1)%moves.length].learningDueTime = new Date('2023-01-21T12:34:57' );
+			moves[(i+1)%moves.length].reviewDueDate = null;
+			moves[(i+2)%moves.length].learningDueTime = new Date('2023-01-21T13:34:56' );
+			moves[(i+2)%moves.length].reviewDueDate = null;
+			moves[(i+3)%moves.length].learningDueTime = null;
+			moves[(i+3)%moves.length].reviewDueDate = new Date('2023-01-20T00:00:00');
+			expect(
+				await getNextLineForStudy( moves, new Date('2023-01-21T12:34:58') )
+			).toMatchObject( {
+				line: [ { id: i } ],
+				start_ix: 0
+			} );
+		}
+	});
 	test.todo( 'no last_line: buildLineBackwards, make sure proper sane line is returned' );
 	test.todo( 'no last_line: continueLineUntilEnd, make sure proper sane line is returned' );
 	test.todo( 'last_line: make sure latest due deviation is returned' );
