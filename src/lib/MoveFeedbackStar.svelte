6a586b914f0bf7c55d6acfb7c7cb84566e831d54
Tim N <tim@gurka.se>
2023-03-25 00:07:35 +0100

    feedback star transition

--- src/lib/MoveFeedbackStar.svelte
+++ src/lib/MoveFeedbackStar.svelte
@@ -4,11 +4,25 @@
 
 	import { createEventDispatcher } from 'svelte';
 	const dispatch = createEventDispatcher();
+
+	function floatAway(node) {
+		return {
+			duration: 3000,
+			css: (t,u) => {
+				let opacity = 1;
+				if ( t < 0.1 ) {
+					opacity = t/0.1;
+				} else if ( t > 0.7 ) {
+					opacity = (1-t)/0.3;
+				}
+				return `opacity: ${opacity}; transform: translateY(${-500*(t)}px)`	
+			}
+		};
+	}
 </script>
 
 <div
-	in:fade="{{ duration: 2000 }}"
-	out:fade="{{ duration: 1000 }}"
+	in:floatAway
 	on:introend="{()=>dispatch('done')}"
 >
 	{content}
@@ -19,5 +33,6 @@
 		width:32px;
 		height:32px;
 		background-color:cyan;
+		z-index:100;
 	}
 </style>
