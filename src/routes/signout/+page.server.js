9fe75f7abf229453efe13932034940ef07f70ace
Tim N <tim@gurka.se>
2023-04-13 10:59:36 +0200

    signout page+button

---
+++ src/routes/signout/+page.server.js
@@ -0,0 +1,11 @@
+import { fail } from "@sveltejs/kit";
+import { auth } from "$lib/server/lucia";
+
+export const actions = {
+	default: async ({ locals }) => {
+		const session = await locals.auth.validate();
+		if (!session) return fail(401);
+		await auth.invalidateSession(session.sessionId); // invalidate session
+		locals.auth.setSession(null); // remove cookie
+	}
+};
---
+++ src/routes/signout/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { enhance } from "$app/forms";
+</script>
+
+<form use:enhance method="post">
+	<input type="submit" class="button" value="Sign out" />
+</form>
