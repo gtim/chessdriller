49fd8a9d82fb56d8b076b91eb85230cd8785b28c
Tim N <tim@gurka.se>
2023-05-06 11:57:05 +0200

    front page max width

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,11 +12,18 @@
 	*/
 </script>
 
-<h1>Hi!</h1>
+<div>
+	<h1>Hi!</h1>
+	<p><b>Chessdriller</b> is an open-source app for spaced repetition of chess openings. It's a free and libre alternative to the spaced-repetition features of Chessable, ChessTempo, Chessmadra, ChessHQ, Chess Position Trainer, Bookup and Listudy.</p>
+	<p>Although I'm building Chessdriller for my own chess practice, it's fully functional. If you've been looking for something like this and would like to try it out, please <a href="mailto:tim@gurka.se">shoot me an email</a> to get an account.</p>
+	<p>Cheers,<br/>
+	Tim</p>
+</div>
 
-<p><b>Chessdriller</b> is an open-source app for spaced repetition of chess openings. It's a free and libre alternative to the spaced-repetition features of Chessable, ChessTempo, Chessmadra, ChessHQ, Chess Position Trainer, Bookup and Listudy.</p>
-
-<p>Although I'm building Chessdriller for my own chess practice, it's fully functional. If you've been looking for something like this and would like to try it out, please <a href="mailto:tim@gurka.se">shoot me an email</a> to get an account.</p>
-
-<p>Cheers,<br/>
-Tim</p>
+<style>
+	div {
+		width:512px;
+		max-width:100%;
+		margin:0 auto;
+	}
+</style>
