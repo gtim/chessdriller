ea2ffdf7af8c0f13f860aed8ad6375b4d6b677c7
Tim N <tim@gurka.se>
2023-05-17 21:12:17 +0200

    remove old test

--- src/routes/+page.server.js
+++
@@ -1,6 +0,0 @@
-export async function load({ params }) {
-
-	//import { PrismaClient } from '@prisma/client';
-	//const prisma = new PrismaClient();
-	return {bla:42};
-}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,3 @@
-<script>
-	export let data;
-	function createTest() {
-		alert(123);
-	}
-	/*
-	export async function load({ fetch, params }) {
-		const res = await fetch(`/api/${params.id}`);
-		const item = await res.json();
-		return { item };
-	}
-	*/
-</script>
-
 <div>
 	<h1>Hi!</h1>
 	<p><b>Chessdriller</b> is an open-source app for spaced repetition of chess openings. It's a free and libre alternative to the spaced-repetition features of Chessable, ChessTempo, Chessmadra, ChessHQ, Chess Position Trainer, Bookup and Listudy.</p>
