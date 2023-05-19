92ed1832f3826b0c390fab9f35279b23a6be2b5f
Tim N <tim@gurka.se>
2023-05-19 11:11:42 +0200

    very simple error page + log hostname on server

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script>
+	export const prerender = true;
+
+	import { page } from '$app/stores';
+
+	console.log($page.status + ' error at ' + $page.url.href);
+</script>
+
+<div class="narrow_container">
+	<h1>Error: {$page.status} {$page.error.message}</h1>
+</div>
+
+<style>
+	.narrow_container {
+		width:512px;
+		max-width:100%;
+		margin:0 auto;
+	}
+</style>
