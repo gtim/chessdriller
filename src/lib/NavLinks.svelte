370850df54b64f7bcb883243835d7e3c40e55dab
Tim N <tim@gurka.se>
2023-05-21 21:46:48 +0200

    mark up NavLinks as a list, "Upload PGN" -> "PGNs"

--- src/lib/NavLinks.svelte
+++ src/lib/NavLinks.svelte
@@ -1,12 +1,25 @@
-<p>
-	<a href="/join">Log in</a>
-	| <a href="/rep">Repertoire</a>
-	| <a href="/pgn">Upload PGN</a>
-	| <a href="/study">Study</a>
-</p>
+<ul>
+	<li><a href="/join">Log in</a></li>
+	<li><a href="/rep">Repertoire</a></li>
+	<li><a href="/pgn">PGNs</a></li>
+	<li><a href="/study">Study</a></li>
+</ul>
 
 <style>
-	p {
-		text-align:center;
+	ul {
+		margin:0;
+		padding:0;
+		display:flex;
+		justify-content:center;
+		list-style-type:none;
+	}
+	li {
+		margin:0;
+		padding:0px 8px;
+	}
+	li + li {
+		border-color:rgba(40,43,40,0.3); /* #282B28 */
+		border-style:solid;
+		border-width:0 0 0 1px;
 	}
 </style>
