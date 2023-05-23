a671d11ef15f7dd1a1998bcf6425678a88e9ee0e
Tim N <tim@gurka.se>
2023-05-23 14:37:07 +0200

    remove unused import + log output

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,9 +1,5 @@
-import { redirect } from "@sveltejs/kit";
-
 export const load = async ({ locals }) => {
 	const { user } = await locals.auth.validateUser();
-	console.log(user);
-
 	return { user };
 };
 
