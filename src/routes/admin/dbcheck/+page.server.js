1f4e24c399faf4da0a0bb15d4fa4abbf29ec6c40
Tim N <tim@gurka.se>
2023-05-27 22:09:36 +0200

    dbcheck: orphan moves that are not deleted

--- src/routes/admin/dbcheck/+page.server.js
+++ src/routes/admin/dbcheck/+page.server.js
@@ -8,6 +8,8 @@ export const load = async ({ locals }) => {
 	if (!user) throw redirect(302, "/");
 	if ( user.cdUserId != 13 ) throw redirect(302, "/"); // TODO hardcoded admin user ID
 
+	let checks = [];
+
 	// Find unincluded studies with moves
 	
 	const unincludedStudiesWithMoves = await prisma.LichessStudy.findMany({
@@ -25,9 +27,33 @@ export const load = async ({ locals }) => {
 			} }
 		}
 	});
+	checks.push( {
+		title: 'Unincluded studies with moves',
+		items: unincludedStudiesWithMoves.map( (s) => `${s.name} (#${s.id}) by ${s.user.lichessUsername}: ${s.moves.length} moves` )
+	} );
 
-	return {
-		unincludedStudiesWithMoves
-	};
+	// Find orphan moves that are not deleted
+	
+	const orphans = await prisma.Move.findMany({
+		where: {
+			AND: [
+				{ pgns:    { none: {} } },
+				{ studies: { none: {} } },
+				{ deleted: false }
+			]
+		},
+		select: {
+			id: true,
+			user: { select: {
+				lichessUsername: true
+			} }
+		}
+	});
+	checks.push( {
+		title: 'Orphan moves that are not deleted',
+		items: orphans.map( (m) => `Move #${m.id} (${m.user.lichessUsername})` )
+	} );
+
+	return { checks };
 
 };
--- src/routes/admin/dbcheck/+page.svelte
+++ src/routes/admin/dbcheck/+page.svelte
@@ -6,17 +6,16 @@
 <div class="narrow_container">
 	<p>This page checks for some specific database inconsistencies that might indicate issues.</p>
 
-	<h2 class:bad={data.unincludedStudiesWithMoves>0} class:good={data.unincludedStudiesWithMoves==0}>
-		Unincluded studies with moves: {data.unincludedStudiesWithMoves.length}
-	</h2>
-	<ul>
-		{#each data.unincludedStudiesWithMoves as study}
-			<li>
-				{study.name} (#{study.id}) by {study.user.lichessUsername} (#{study.user.id}):
-				{study.moves.length} moves ({study.moves.filter((m)=>m.deleted).length} soft-deleted).
-			</li>
-		{/each}
-	</ul>
+	{#each data.checks as check}
+		<h2 class:bad={check.items.length>0} class:good={check.items.length==0}>
+			{check.title}: {check.items.length}
+		</h2>
+		<ul>
+			{#each check.items as item}
+				<li>{item}</li>
+			{/each}
+		</ul>
+	{/each}
 </div>
 
 <style>
