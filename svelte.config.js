ee840feb916d4cc328ec87fdc619b74afbee465f
Tim N <tim@gurka.se>
2023-07-07 12:58:50 +0200

    typescript support for typescript files

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,9 @@
 import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter()
 	}
