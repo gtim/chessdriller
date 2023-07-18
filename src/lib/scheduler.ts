fcae2c04a0117b25b5d475c550959ca9f0289811
Tim N <tim@gurka.se>
2023-07-18 23:04:35 +0200

    fix lines occasionally starting too advanced (fixes #51)

--- src/lib/scheduler.ts
+++ src/lib/scheduler.ts
@@ -85,9 +85,10 @@ export async function getLineForStudy( repertoire: Move[], now: Date, lastLineId
 		const repForWhite = lastFinalMove.repForWhite;
 		const continuation = bfsDueContinuationToEnd( repertoire, now, currentFen, repForWhite, nextMoveIsOwn, new Set(lastLineIds) );
 		if ( continuation.length > 0 ) {
+			let start_ix = line.length;
 			line.push( ...continuation );
 			const due_ix = lineToDueIx(line,now);
-			const start_ix = Math.min( line.length, ...due_ix );
+			start_ix = Math.min( start_ix, ...due_ix );
 			return { line, start_ix, due_ix, num_due_moves };
 		}
 	}
