fffe67ceefd259aefc3812a93b171a003fe7ae84
Tim N <tim@gurka.se>
2023-07-21 15:10:35 +0200

    open feedback modal (unsubmittable)

---
+++ src/lib/FeedbackModal.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+	import { closeModal } from 'svelte-modals'
+	import { fly } from 'svelte/transition'
+
+	export let isOpen: boolean;
+</script>
+
+{#if isOpen}
+	<!-- on:introstart and on:outroend are required to transition 1 at a time between modals -->
+	<div role="dialog" class="modal" transition:fly={{ y: 50 }} on:introstart on:outroend>
+		<div class="contents">
+			<h2>Feedback</h2>
+			<div>
+				<p>Any feedback is greatly appreciated.</p>
+				<textarea placeholder="feedback, question or bug report"></textarea>
+			</div>
+			<div>
+				<p>Email address if you'd like a respose:</p>
+				<input type="text" class="email" placeholder="email address">
+			</div>
+			<input type="submit" class="submit cdbutton" value="Send feedback">
+		</div>
+	</div>
+{/if}
+
+<style>
+	.modal {
+		position: fixed;
+		top: 0;
+		bottom: 0;
+		right: 0;
+		left: 0;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		z-index:10;
+		/* allow click-through to backdrop */
+		pointer-events: none;
+	}
+
+	.contents {
+		min-width: 240px;
+		max-width: 512px;
+		border-radius: 6px;
+		padding: 16px;
+		background-color: #FAF0E6;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		gap:8px;
+		pointer-events: auto;
+	}
+	
+	textarea {
+		height:96px;
+	}
+	textarea, input.email {
+		box-sizing: border-box;
+		width:100%;
+	}
+	input.submit {
+		align-self:center;
+		width:fit-content;
+		margin-top:8px;
+		font-size:14px;
+		padding:4px 8px;
+	}
+
+	h2 {
+		text-align:center;
+		margin:0;
+	}
+	p {
+		margin:0 0 4px 0;
+	}
+
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,9 @@
 	import { Modals, closeModal } from 'svelte-modals'
 	import { fade } from 'svelte/transition';
 
+	import FeedbackModal from '$lib/FeedbackModal.svelte'
+	import { openModal } from 'svelte-modals'
+
 	export let data;
 	$: loggedIn = !!data.user;
 </script>
@@ -22,7 +25,7 @@
 		<ul>
 			<li>
 				<!-- feedback SVG from Remixicon -->
-				<a href="/">
+				<a on:click={()=>openModal(FeedbackModal)}>
 					Feedback
 					<svg style="height:18px;vertical-align:middle;margin-bottom:2px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 						<path fill="#800020" d="M6.45455 19L2 22.5V4C2 3.44772 2.44772 3 3 3H21C21.5523 3 22 3.44772 22 4V18C22 18.5523 21.5523 19 21 19H6.45455ZM4 18.3851L5.76282 17H20V5H4V18.3851ZM11 13H13V15H11V13ZM11 7H13V12H11V7Z"></path>
