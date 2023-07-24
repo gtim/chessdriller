fb8f3bb72492d8902f4d5d00dfba340392ce94cd
Tim N <tim@gurka.se>
2023-07-24 22:36:54 +0200

    open lichess/wikipedia links in new window

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -2,7 +2,7 @@
 	<h1>Frequently Asked Questions</h1>
 	<div class="entry">
 		<p class="question">How do I create a study in the Lichess app?</p>
-		<p class="answer">You'll have to use the <a href="https://lichess.org/study">Lichess web interface</a> to create a study. The Lichess app lacks many features, but they're working on a new one.</p>
+		<p class="answer">You'll have to use the <a href="https://lichess.org/study" target="_blank" rel="noopener noreferrer">Lichess web interface</a> to create a study. The Lichess app lacks many features, but they're working on a new one.</p>
 	</div>
 	<div class="entry">
 		<p class="question">Can Chessdriller import Chessable courses?</p>
@@ -18,7 +18,7 @@
 	</div>
 	<div class="entry">
 		<p class="question">What does "<i>x</i> due moves" mean in the study screen?</p>
-		<p class="answer">There's <i>x</i> moves ready to practice. Play through your repertoire against Chessdriller to get to them. When you first add a move, it will be due immediately. After playing a move correctly for the first time, it will be due again after 10 minutes. Each time the move is played correctly, the interval increases, approximately: 10 minutes, one hour, 8 hours, 1 day, 3 days, 7 days... Playing a move incorrectly resets the interval. This science-based learning technique is called <a href="https://en.wikipedia.org/wiki/Spaced_repetition">spaced repetition</a>.</p>
+		<p class="answer">There's <i>x</i> moves ready to practice. Play through your repertoire against Chessdriller to get to them. When you first add a move, it will be due immediately. After playing a move correctly for the first time, it will be due again after 10 minutes. Each time the move is played correctly, the interval increases, approximately: 10 minutes, one hour, 8 hours, 1 day, 3 days, 7 days... Playing a move incorrectly resets the interval. This science-based learning technique is called <a href="https://en.wikipedia.org/wiki/Spaced_repetition" target="_blank" rel="noopener noreferrer">spaced repetition</a>.</p>
 	</div>
 </div>
 
