44e47a6e05eeddf3debd66f1c9ec7543d30e83a1
Tim N <tim@gurka.se>
2023-10-06 12:43:17 +0200

    link to data export (#86)

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -9,6 +9,7 @@
 	<form use:enhance method="post">
 		<input type="submit" class="cdbutton" value="Sign out" />
 	</form>
+	<p>You can <a href="/settings/export">export your data</a> any time you like.</p>
 </div>
 
 <style>
